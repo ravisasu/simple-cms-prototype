467a86e58088bbf40dfdc8bac1d8ac76084deff6
ravisasu <ravichandrasasu@gmail.com>
2025-11-22 13:30:39 +0530

    Remove deprecated stage-folder helper scripts

--- simple-cms-prototype/scripts/move_to_stage.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-def main():
-    print("Deprecated: 'move_to_stage.py' has been retired. Use 'scripts/workflow_transition.py <slug> <stage>' instead (only Approved triggers publish move).")
-    print("No action taken.")
-    return 0
-
-if __name__ == '__main__':
-    sys.exit(main())
